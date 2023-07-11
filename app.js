9bb6f0411878bfe0fa031480448dca8fbf0e0725
vgnsh1999 <vigneshvarathan@gmail.com>
2023-07-11 16:04:33 +0530

    After cleaned up

--- app.js
+++ app.js
@@ -11,41 +11,9 @@ app.use(cors());
 
 app.use(bodyParser.json({extended:false}));
 
-app.post('/expense/add-expense',async (req,res,next)=>{
-    try{
-        console.log('hi')
-        const amount = req.body.amount;
-        const description = req.body.description;
-        const category = req.body.category;
+const expenseRoutes = require('./routes/expense');
 
-        const data = await Expense.create({amount:amount,description:description,category:category}); 
-        res.status(201).json({newExpenseAdded:data});
-    } catch(error){
-        console.log(JSON.stringify(error));
-        res.status(500).json({error:error});
-    }
-});
-
-app.get('/expense/get-expense',async (req,res,next)=>{
-    try{
-        const expenses = await Expense.findAll();
-        res.status(200).json({allExpenses:expenses});
-    } catch(error){
-        console.log(JSON.stringify(error));
-        res.status(500).json({error:error});
-    }
-});
-
-app.delete('/expense/delete-expense/:id',async (req,res,next)=>{
-    try{
-        const expenseID = req.params.id;
-        await Expense.destroy({where:{id:expenseID}});
-        res.sendStatus(200);
-    } catch(error){
-        console.log(JSON.stringify(error));
-        res.status(500).json({error:error});
-    }
-});
+app.use('/expense',expenseRoutes);
 
 sequelize.sync().then((response) =>{
     console.log(response);
--- routes/expense.js
+++ routes/expense.js
@@ -2,5 +2,12 @@ const express = require('express');
 
 const router = express.Router();
 
+const expenseController = require('../controllers/expensecontrollers');
+
+router.post('/add-expense',expenseController.addExpense);
+
+router.get('/get-expense',expenseController.getExpense);
+
+router.delete('/delete-expense/:id',expenseController.deleteExpense);
 
 module.exports = router;
